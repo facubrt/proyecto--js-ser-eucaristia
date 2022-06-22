570ccdc4050df3484dc91340df1e0b1d0f71a33d
Facundo Barreto <facubrt@gmail.com>
2022-06-21 22:40:30 -0300

    se realiza la entrega del desafío complementario de la clase 08 donde se interactúa con el HTML a través del DOM

--- js/script.js
+++ js/script.js
@@ -11,61 +11,61 @@ const paperplanes = [
         quote: "Al final del camino me dirán: ¿Has vivido? ¿Has amado? Y yo, sin decir nada, abriré el corazón lleno de nombres",
         source: "Pedro Casaldáliga",
         category: "Amor",
-        user: "Valentina",
+        img: "img-01",
     },
     {
         quote: "Y me dijo el Señor que quería que yo fuera un nuevo loco en el mundo",
         source: "San Francisco de Asís",
         category: "Entrega",
-        user: "Horacio",
+        img: "img-02",
     },
     {
         quote: "Rezar el Rosario es contemplar con María el rostro de Cristo",
         source: "San Juan Pablo II",
         category: "María",
-        user: "Josefina"
+        img: "img-03"
     },
     {
         quote: "En su bondad infinita, jamás abandona Dios a aquellos que no le quieren abandonar a Él.",
         source: "San Francisco de Sales",
         category: "Amor",
-        user: "Facundo",
+        img: "img-04",
     },
     {
         quote: "La santidad no es otra cosa sino una respuesta de amor al anuncio del amor",
         source: "Padre Ignacio Larrañaga",
         category: "Santidad",
-        user: "Valentina"
+        img: "img-05"
     },
     {
         quote: "Dios extiende sus manos en la Cruz para abrazar hasta los confines del universo",
         source: "San Cirilo de Jerusalén ",
         category: "Amor",
-        user: "Joaquín"
+        img: "img-06"
     },
     {
         quote: "Ser libre en el amor es su completa y total realización",
         source: "Viviana Ruffener",
         category: "Amor",
-        user: "Lucía"
+        img: "img-07"
     },
     {
         quote: "Te buscaba afuera, Señor, y tú estabas dentro de mi, en mi corazón...",
         source: "San Agustín",
         category: "Amor",
-        user: "Enzo"
+        img: "img-08"
     },
     {
         quote: "Mira la estrella e invoca a María, porque un hijo de María nunca perecedera",
         source: "San Bernardo",
         category: "María",
-        user: "Facundo"
+        img: "img-09"
     },
     {
         quote: "La paz comienza con una sonrisa",
         source: "Santa Teresa de Calcuta",
         category: "Santidad",
-        user: "Estefanía"
+        img: "img-10"
     },
 ]
 
@@ -74,11 +74,12 @@ class Paperplane {
         this.quote = paperplane.quote;
         this.source = paperplane.source;
         this.category = paperplane.category;
-        this.user = paperplane.user;
+        this.img = paperplane.img;
     }
 
     getPaperplane() {
-        alert(`El avioncito que recibiste contiene las siguientes palabras: \n\n"${this.quote}"\n\n${this.source} - ${this.category}\nAvioncito construido por ${this.user}`)
+        alert(`El avioncito que recibiste contiene las siguientes palabras: \n\n"${this.quote}"\n\n${this.source} - ${this.category}`)
+
     }
 
 }
@@ -93,7 +94,25 @@ for (const paperplane of paperplanes) {
 // GENERACION ALEATORIA DE AVIONCITO
 function surprise() {
     let index = Math.round(Math.random() * paperplanesList.length);
-    paperplanesList[index].getPaperplane();
+    //paperplanesList[index].getPaperplane();
+
+    //innerHTML
+    let paperplane = document.querySelector('.paperplane');
+    let imgPaperplane = document.createElement('div');
+    let txtPaperplane = document.createElement('div');
+    paperplane.innerHTML = `
+    <div class="imgPaperplane">
+
+    <img src="assets/img/${paperplanesList[index].img}.png">
+    </div>
+    <div class="txtPaperplane">
+    <h1>${paperplanesList[index].quote}</h1>
+    <h2>${paperplanesList[index].source}</h2>
+    <h3>${paperplanesList[index].category}</h3>
+    </div>
+    `;
+
+    
 }
 
 function filtCategory() {
@@ -101,7 +120,7 @@ function filtCategory() {
     let paperplane;
     switch (comando) {
         case "AMOR":
-            paperplane = new Paperplane(paperplanes.find(paperplane => paperplane.category === 'Amor')); 
+            paperplane = new Paperplane(paperplanes.find(paperplane => paperplane.category === 'Amor'));
             paperplane.getPaperplane();
             break;
         case "MARIA":
